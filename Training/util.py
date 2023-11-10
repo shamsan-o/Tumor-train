d9df23827414a0c841a210e895e272e540aabcf6
Shamsan_O <72396811+shamsan-o@users.noreply.github.com>
2023-11-10 14:04:47 +0530

    Update util.py

--- Training/util.py
+++ Training/util.py
@@ -12,22 +12,7 @@ import cv2
 
 
 def get_cfg(output_dir, learning_rate, batch_size, iterations, checkpoint_period, model, device, nmr_classes):
-    """
-    Create a Detectron2 configuration object and set its attributes.
-
-    Args:
-        output_dir (str): The path to the output directory where the trained model and logs will be saved.
-        learning_rate (float): The learning rate for the optimizer.
-        batch_size (int): The batch size used during training.
-        iterations (int): The maximum number of training iterations.
-        checkpoint_period (int): The number of iterations between consecutive checkpoints.
-        model (str): The name of the model to use, which should be one of the models available in Detectron2's model zoo.
-        device (str): The device to use for training, which should be 'cpu' or 'cuda'.
-        nmr_classes (int): The number of classes in the dataset.
-
-    Returns:
-        The Detectron2 configuration object.
-    """
+    
     cfg = _get_cfg()
 
     # Merge the model's default configuration file with the default Detectron2 configuration file.
@@ -76,28 +61,7 @@ def get_cfg(output_dir, learning_rate, batch_size, iterations, checkpoint_period
 
 
 def get_dicts(img_dir, ann_dir):
-    """
-    Read the annotations for the dataset in YOLO format and create a list of dictionaries containing information for each
-    image.
-
-    Args:
-        img_dir (str): Directory containing the images.
-        ann_dir (str): Directory containing the annotations.
-
-    Returns:
-        list[dict]: A list of dictionaries containing information for each image. Each dictionary has the following keys:
-            - file_name: The path to the image file.
-            - image_id: The unique identifier for the image.
-            - height: The height of the image in pixels.
-            - width: The width of the image in pixels.
-            - annotations: A list of dictionaries, one for each object in the image, containing the following keys:
-                - bbox: A list of four integers [x0, y0, w, h] representing the bounding box of the object in the image,
-                        where (x0, y0) is the top-left corner and (w, h) are the width and height of the bounding box,
-                        respectively.
-                - bbox_mode: A constant from the `BoxMode` class indicating the format of the bounding box coordinates
-                             (e.g., `BoxMode.XYWH_ABS` for absolute coordinates in the format [x0, y0, w, h]).
-                - category_id: The integer ID of the object's class.
-    """
+    
     dataset_dicts = []
     for idx, file in enumerate(os.listdir(ann_dir)):
         # annotations should be provided in yolo format
@@ -136,16 +100,7 @@ def get_dicts(img_dir, ann_dir):
 
 
 def register_datasets(root_dir, class_list_file):
-    """
-    Registers the train and validation datasets and returns the number of classes.
-
-    Args:
-        root_dir (str): Path to the root directory of the dataset.
-        class_list_file (str): Path to the file containing the list of class names.
-
-    Returns:
-        int: The number of classes in the dataset.
-    """
+    
     # Read the list of class names from the class list file.
     with open(class_list_file, 'r') as reader:
         classes_ = [l[:-1] for l in reader.readlines()]
@@ -162,23 +117,7 @@ def register_datasets(root_dir, class_list_file):
 
 def train(output_dir, data_dir, class_list_file, learning_rate, batch_size, iterations, checkpoint_period, device,
           model):
-    """
-    Train a Detectron2 model on a custom dataset.
-
-    Args:
-        output_dir (str): Path to the directory to save the trained model and output files.
-        data_dir (str): Path to the directory containing the dataset.
-        class_list_file (str): Path to the file containing the list of class names in the dataset.
-        learning_rate (float): Learning rate for the optimizer.
-        batch_size (int): Batch size for training.
-        iterations (int): Maximum number of training iterations.
-        checkpoint_period (int): Number of iterations after which to save a checkpoint of the model.
-        device (str): Device to use for training (e.g., 'cpu' or 'cuda').
-        model (str): Name of the model configuration to use. Must be a key in the Detectron2 model zoo.
-
-    Returns:
-        None
-    """
+    
 
     # Register the dataset and get the number of classes
     nmr_classes = register_datasets(data_dir, class_list_file)
